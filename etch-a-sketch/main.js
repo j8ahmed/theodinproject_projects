8631ca4289ee8c6ced850ee9b92ac486483d81bd
Jamal Ahmed <j8ahmed@gmail.com>
2022-03-23 18:03:42 -0400

    Added the clear button and it's functionality.

--- etch-a-sketch/main.js
+++ etch-a-sketch/main.js
@@ -9,18 +9,23 @@ function etchASketch(){
   const menu = document.querySelector("#menu");
   const canvas = document.querySelector("#canvas");
   const canvasSize = canvas.clientWidth;
+  const clearBtn = document.querySelector("#clear-btn");
+  clearBtn.addEventListener("click", clearCanvas);
   const units = 16;
   const gapWidth = 1;
 
-  for(let i=0; i < units; ++i){
-    for(let j=0; j < units; ++j){
-      const pixel = document.createElement("div");
-      pixel.classList.add("canvas-pixel");
-      pixel.setAttribute("id", `pixel-r${i+1}-c${j+1}`);
-      pixel.style.width = `${canvasSize.x}px`;
-      pixel.style.height = `${canvasSize.y}px`;
+  function constructCanvas(){
+    for(let i=0; i < units; ++i){
+      for(let j=0; j < units; ++j){
+	const pixel = document.createElement("div");
+	pixel.classList.add("canvas-pixel");
+	pixel.setAttribute("id", `pixel-r${i+1}-c${j+1}`);
+	pixel.addEventListener("mouseover", fillUnit);
+	pixel.style.width = `${canvasSize.x}px`;
+	pixel.style.height = `${canvasSize.y}px`;
 
-      canvas.appendChild(pixel);
+	canvas.appendChild(pixel);
+      }
     }
   }
 
@@ -32,7 +37,22 @@ function etchASketch(){
     canvas.style.gridTemplateColumns = `repeat(${units}, minmax(${unitSize}px, 1fr))`;
   }
 
-  setCanvasDimensions();
+  function fillUnit(e){
+    let unit = e.currentTarget;
+    unit.style.background = "black";
+  }
 
+  function clearCanvas(){
+    // Clear all pixel units in the Canvas 
+    // Set background to white or none
+    for(const pixel of canvas.children){
+      pixel.style.background = "";
+    }
+  }
+
+
+  constructCanvas();
+  setCanvasDimensions();
 }
+
 etchASketch();
