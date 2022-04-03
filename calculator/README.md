eb333b6ce04c8343365fe29170e1a8ebba4423f4
Jamal Ahmed <j8ahmed@gmail.com>
2022-04-02 20:31:00 -0400

    Built majority of calculator internal functions

--- calculator/README.md
+++ calculator/README.md
@@ -8,21 +8,34 @@
 - [x] Finish basic outline design of project (Containers for UI)
 - [x] Set up files and links between them
 - [x] Add rough HTML layout of elements (display, buttons)
-- [ ] Plan and Outline functionality of code (How will the calculator work)
-- [ ] Build out arithmetic functions (add, subtract, multiply, divide)
-- [ ] Build out 
-- [ ] 
-- [ ] 
+- [x] Plan and Outline functionality of code (How will the calculator work)
+- [x] Build out arithmetic functions (add, subtract, multiply, divide)
+- [ ] Build out core functions for calculator
+  - [x] Build update state function by matching staging values and checking for differences (Type check is a bonus)
+  - [x] Build button / decimal button callback function for their respective event listeners
+    - [x] Build decimalCheck function to prevent multiple decimals inserted into Calculator
+    - [x] Build updateDisplay function to handle display calls.
+  - [x] Build operator button callback function for their respective event listeners
+
+- [x] Build initialization code to connect elements to event listeners to make the calculator work
+  - [x] Had to update HTML to includes classes to get all related UI
+  - [x] Had to fix bug for pulling `<td>` element value
+  - [x] Had to add case for the base case of the Calculator Display showing "0"
+  - [x] Fix bug where second operand is being appended instead of replacing display
 - [ ] 
 - [ ] 
 - [ ] 
 - [ ] 
 - [ ] 
 
-# Bonus To Do Items
+
+### Bonus To Do Items
 
 - [ ] 
 
+## Tests
+
+1. `1 + 5 = 6`
 
 ## Core Operations:
 
@@ -52,7 +65,7 @@ Options while inputting the operator:
 
   - Change the operator
   - Pressing the operator button multiple times (Should just change the operator)
-  - 
+  - Evaluating the previous operation while preparing the next opartion e.g. `1 + 2 - 1 + 5 - 2`
 
 Options while inputting the second number:
 
@@ -86,8 +99,13 @@ Cool Submissions 1
 ## Bug Fixes
 
 - [Table data span 2 columns](https://www.w3schools.com/tags/att_td_colspan.asp)
-- []()
-- []()
+- [For of loop docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of)
+- [For Each loop for arrays](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
+- [How to check if property exists](https://stackoverflow.com/questions/455338/how-do-i-check-if-an-object-has-a-key-in-javascript)
+- [Javascript prop 'in' object operator](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/in)
+- [Jump to Function definition in Vim](https://stackoverflow.com/questions/635770/jump-to-function-definition)
+- [Parsing String in JavaScript](https://dev.to/sanchithasr/7-ways-to-convert-a-string-to-number-in-javascript-4l)
+- [Get td text value](https://stackoverflow.com/questions/2310145/javascript-getting-value-of-a-td-with-id-name)
 - []()
 - []()
 - []()
--- calculator/main.js
+++ calculator/main.js
@@ -1,6 +1,161 @@
 /* 
  * Code Outline:
+ * 
+ * calcultor()
  *
+ *   Core:
+ *     - init();
+ *     - updateState(newState)
+ *     - updateStaging(StagingValues)
+ *     - operation(n1, n2, op)
+ *     - numDecPress(e)
+ *     - operatorPress(e)
+ *
+ *   Utility:
+ *     - add()
+ *     - subtract()
+ *     - multiply()
+ *     - divide()
+ *     - reset()
+ *     - clear()
+ *     - decimalCheck();
+ *     - updateDisplay();        // Updates state and Element 
+ *     - updateUI();             // Updates UI with state
  *
   */
 
+
+function calculator(){
+  const numBtns = document.getElementsByClassName("num-btn");
+  const opBtns = document.getElementsByClassName("op-btn");
+  const specialBtns = document.getElementsByClassName("special-btn");
+  const display = document.getElementById("display");
+  const state = {
+    firstOperand: 0,
+    currOperator: "+",
+    rTotal: 0,             // number
+    display: "0",          // string
+  }
+  const stagingValues = {
+    ready: false,
+  }
+
+  // Core functions
+
+  function init(){
+    // Add Event Listeners
+    Array.from(numBtns).forEach(
+      element => element.addEventListener("click", numDecPress));
+    Array.from(opBtns).forEach(
+      element => element.addEventListener("click", operatorPress));
+    updateUI();
+  }
+
+  function updateState(){
+    Object.keys(stagingValues).forEach(prop => {
+      if(prop in state && stagingValues[prop] !== state[prop]){
+	state[prop] = stagingValues[prop];
+      }  
+    });
+  }
+
+  function updateStaging(name, value){
+    stagingValues[name] = value;
+  }
+
+  function operation(n1, n2, op){
+    switch(op){
+      case "+":
+	return add(n1, n2);
+
+      case "-":
+	return subtract(n1, n2);
+
+      case "*":
+	return multiply(n1, n2);
+
+      case "/":
+	return divide(n1, n2);
+
+      default:
+	return add(n1, n2);
+    }
+  }
+
+  function numDecPress(e){
+    let startSecondOperand = false;    
+    const btnVal = e.target.textContent;
+    // Push any values in staging to state
+    if(stagingValues.ready){
+      updateState();
+      updateStaging("ready", false);
+      startSecondOperand = true;
+    } 
+
+    // Handle decimal input
+    if(btnVal === "."){
+      if(decimalCheck()) 
+	return
+      else{
+	updateDisplay(state.display + ".");
+	return
+      } 
+    }
+    // Handle reset case Display has only "0"
+    if(state.display === "0"){
+      console.log(btnVal)
+      updateDisplay(btnVal);
+    }
+    else if(startSecondOperand)
+      updateDisplay(btnVal);
+    else
+      updateDisplay(state.display + btnVal);
+  }
+
+  function operatorPress(e){
+    if(state.firstOperand !== 0 && state.currOperator !== ""){
+      // Perform operation and show result
+      const result = operation(
+	Number(state.firstOperand), 
+	Number(state.display), 
+	state.currOperator
+      );
+      updateDisplay(result);
+    }
+
+    // Send updated values to staging to wait for Confirmation
+    updateStaging("currOperator", e.target.textContent);
+    updateStaging("firstOperand", state.display);
+    updateStaging("ready", true);
+  }
+
+  // Utility functions
+  function add(n1, n2){ return n1 + n2 }
+  function subtract(n1, n2){ return n1 - n2 }
+  function multiply(n1, n2){ return n1 * n2 }
+  function divide(n1, n2){ return n1 / n2 }
+  function reset(){}
+  function clear(){}
+  function decimalCheck(){ 
+    const regx = new RegExp(state.display);
+    return regx.test(".") 
+  }
+
+  function updateDisplay(value){
+    state.display = String(value);
+    updateUI();
+  }
+
+  function updateUI(){
+    display.textContent = state.display;
+
+    // For Testing purposes
+    Object.keys(state).map(
+      prop => console.log(`${prop}: ${state[prop]}\n`))
+    console.log("---------")
+  }
+
+  init()                    // Initialize calculator
+}
+
+calculator();
