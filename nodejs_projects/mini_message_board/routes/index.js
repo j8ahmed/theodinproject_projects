6bc6c8ebff968d6ef9c87e7d70d21017dfe4378c
Jamal Ahmed <j8ahmed@gmail.com>
2022-07-01 01:41:47 -0400

    Construct Mini Message Board app for Heroku deployment.

---
+++ nodejs_projects/mini_message_board/routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
+
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Message Board App', messages })
+})
+
+router.get('/new', (req, res, next) => {
+  res.render('form', {title: 'Add a Message'})
+})
+
+
+router.post('/new', (req, res, next) => {
+  const {message, username} = req.body
+  messages.push({text: message, user: username, added: new Date()})
+  res.redirect('/')
+})
+
+
+
+
+module.exports = router;
