656e1e8673db6ff29804bea0cf4cde762874b3da
Jamal Ahmed <j8ahmed@gmail.com>
2022-07-20 18:47:36 -0400

    Add error handler middleware

--- nodejs_projects/inventory_application/app.js
+++ nodejs_projects/inventory_application/app.js
@@ -37,11 +37,19 @@ app.use('/', indexRouter)
 app.use('/items', itemRouter)
 app.use('/categories', categoryRouter)
 
+// Catch all 404 errors and forward to error handler
+app.use((req, res, next) => { next(createError(404)) })
 
+//Error Handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-
-
-
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 
 module.exports = app;
